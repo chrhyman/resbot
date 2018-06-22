1cbb5e9d45ecb7858aa4cfe2f6a818f613be4708
Chris Hyman <chris@wugs.me>
2018-06-22 13:19:20 -0700

    Add files via upload
    Seeing what changes have been made from messy updates

--- classes.py
+++ classes.py
@@ -1,23 +1,21 @@
 from constants import *
 
-# handles number of players, spies, resistance, and team sizes/fails needed
+# class to handle number of players and quantities of them in game
 class Number:
     MINPLAYERS = 5
     MAXPLAYERS = 10
     def __init__(self, players):
         min, max = Number.MINPLAYERS, Number.MAXPLAYERS
-        assert min <= players <= max, 'Error: Invalid player count'
+        assert min <= players <= max, 'Error: Must have {0}-{1} players.'.format(min, max)
         self.players = players
-        self.spies = self.get_spies()
-        self.res = self.get_res()
 
-    def get_spies(self):     # 1/3 of all players are spies (round up)
+    def getSpies(self):     # 1/3 of all players are spies (round up)
         return -(-self.players//3)
 
-    def get_res(self):       # all players that aren't spies are res
-        return self.players - self.spies
+    def getRes(self):       # all players that aren't spies are res
+        return self.players - self.getSpies()
 
-    def team_size(self, mission):     # static from game rules; mission is int
+    def getTeamSize(self, mission):     # static from game rules
         if mission == 1:
             if self.players <= 7:   return 2
             else:                   return 3
@@ -37,9 +35,9 @@ class Number:
             elif self.players <= 7: return 4
             else:                   return 5
         else:
-            assert False, 'Error: Mission %s does not exist' % str(mission)
+            assert False, 'Error: Mission number %s does not exist' % mission
 
-    def fails_needed(self, mission):  # mission 4 needs two fails with 7+
+    def getFailsNeeded(self, mission):  # mission 4 needs two fails with 7+
         if mission == 4 and self.players >= 7:
             return 2
         else:
@@ -52,36 +50,36 @@ class Role:
         self.role = role
         # resistance roles
         if self.role in RESROLES:
-            self.is_res = True              # True for Res only
-            self.can_shoot = False          # True for Assassin
-            self.spy_seen_by_spies = False  # True for non-Oberon spies
-            self.spy_seen_by_merl = False   # True for non-Mordred spies
-            self.can_see_spies = False      # True for Merlin & non-Ob spies
-            self.looks_like_merl = False    # True for Merlin and Morgana
-            self.can_see_merl = False       # True for Percival
+            self.isRes = True           # flag True for Res only
+            self.canShoot = False       # flag True for Assassin
+            self.spySeenBySpies = False # flag True for non-Oberon spies
+            self.spySeenByMerl = False  # flag True for non-Mordred spies
+            self.canSeeSpies = False    # flag True for Merlin and non-Ob spies
+            self.looksLikeMerl = False  # flag True for Merlin and Morgana
+            self.canSeeMerl = False     # flag True for Percival
             if self.role == MERLIN:
-                self.can_see_spies = True
-                self.looks_like_merl = True
+                self.canSeeSpies = True
+                self.looksLikeMerl = True
             if self.role == PERCIVAL:
-                self.can_see_merl = True
+                self.canSeeMerl = True
         # spy roles
         if self.role in SPYROLES:
-            self.is_res = False
-            self.can_shoot = False
-            self.spy_seen_by_spies = True
-            self.spy_seen_by_merl = True
-            self.can_see_spies = True
-            self.looks_like_merl = False
-            self.can_see_merl = False
+            self.isRes = False
+            self.canShoot = False
+            self.spySeenBySpies = True
+            self.spySeenByMerl = True
+            self.canSeeSpies = True
+            self.looksLikeMerl = False
+            self.canSeeMerl = False
             if self.role in (ASSASSIN, MORDASS):
-                self.can_shoot = True
+                self.canShoot = True
             if self.role == MORGANA:
-                self.looks_like_merl = True
+                self.looksLikeMerl = True
             if self.role in (MORDRED, MORDASS):
-                self.spy_seen_by_merl = False
+                self.spySeenByMerl = False
             if self.role == OBERON:
-                self.spy_seen_by_spies = False
-                self.can_see_spies = False
+                self.spySeenBySpies = False
+                self.canSeeSpies = False
 
     def __str__(self):
         return self.role
---
+++ privdata.py
@@ -0,0 +1,2 @@
+TOKEN = 'NDQwNDAwMjcxOTc0MDcyMzIx.DchKqQ.qlIv6WEsagA5MH52VV_IPnQjeAU'
+ADMIN_IDS = [290518243188146186] # must be type List, items are type Int
