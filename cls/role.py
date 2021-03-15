7cef5633b0e72acd5ed8cd30e625b7feb8420852
chrhyman <chris@wugs.me>
2021-03-15 01:39:01 -0700

    Update role.py
    added docstring explanations for each role

--- cls/role.py
+++ cls/role.py
@@ -1,10 +1,12 @@
 import txt.roles as R
 
 class Role:
+    '''Base class for all Roles'''
     def __str__(self):
         return self.role
 
-class Leader:
+class Leader(Role):
+    '''Visible to Percival'''
     pass
 
 class Res(Role):
@@ -15,14 +17,17 @@ class VanRes(Res):
         self.role = R.VANRES
 
 class Merlin(Res, Leader):
+    '''Can see all non-Hidden Spies'''
     def __init__(self):
         self.role = R.MERLIN
 
 class Percival(Res):
+    '''Can identify Leaders, one of whom is Merlin'''
     def __init__(self):
         self.role = R.PERCIVAL
 
 class Spy(Role):
+    '''Can see all non-Oberon Spies'''
     pass
 
 class VanSpy(Spy):
@@ -30,6 +35,7 @@ class VanSpy(Spy):
         self.role = R.VANSPY
 
 class Shooter(Spy):
+    '''Can attempt to shoot Merlin at the end of the game'''
     pass
 
 class Assassin(Shooter):
@@ -37,10 +43,12 @@ class Assassin(Shooter):
         self.role = R.ASSASSIN
 
 class Morgana(Spy, Leader):
+    '''Looks like Merlin to Percival'''
     def __init__(self):
         self.role = R.MORGANA
 
 class Hidden(Spy):
+    '''Cannot be seen by Merlin'''
     pass
 
 class Mordred(Hidden):
@@ -52,5 +60,6 @@ class MordAss(Hidden, Shooter):
         self.role = R.MORDASS
 
 class Oberon(Spy):
+    '''Cannot be seen by other Spies, but can be seen by Merlin'''
     def __init__(self):
         self.role = R.OBERON
