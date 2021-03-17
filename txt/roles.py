39f4f13ae88384fe85af931557448ed917d0cf21
chrhyman <chris@wugs.me>
2021-03-16 19:17:39 -0700

    Update roles.py
    i see no need for these lists to check membership since we have the Role class

--- txt/roles.py
+++ txt/roles.py
@@ -2,7 +2,6 @@ RES = 'the RESISTANCE'                      # team name
 VANRES = 'a member of the RESISTANCE'       # VAN = vanilla, i.e. normal
 MERLIN = 'Merlin'
 PERCIVAL = 'Percival'
-res_roles = (VANRES, MERLIN, PERCIVAL)
 
 SPY = 'the SPIES'                           # team name
 VANSPY = 'a SPY'                            # VAN = vanilla, i.e. normal
@@ -11,13 +10,6 @@ MORGANA = 'Morgana'
 MORDRED = 'Mordred'
 MORDASS = 'Mordred the Assassin'
 OBERON = 'Oberon'
-spy_roles = (VANSPY, ASSASSIN, MORGANA, MORDRED, MORDASS, OBERON)
 
-van_roles = (VANRES, VANSPY)
 VANRES_PL = 'members of the RESISTANCE'
 VANSPY_PL = 'SPIES'
-
-roles = (
-    VANRES, MERLIN, PERCIVAL,
-    VANSPY, ASSASSIN, MORGANA, MORDRED, MORDASS, OBERON
-)
