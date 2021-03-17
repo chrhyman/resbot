7a93b48222c0f97628537722950bf1424e0b5d41
chrhyman <chris@wugs.me>
2021-03-16 17:23:23 -0700

    Update role.py
    bool methods with isinstance instead of clunky if/else trees. inheritance makes this relationship much clearer

--- cls/role.py
+++ cls/role.py
@@ -5,6 +5,30 @@ class Role:
     def __str__(self):
         return self.role
 
+    def is_res(self):
+        return isinstance(self, Res)
+
+    def is_spy(self):
+        return isinstance(self, Spy)
+
+    def is_leader(self):
+        return isinstance(self, Leader)
+
+    def is_shooter(self):
+        return isinstance(self, Shooter)
+
+    def is_hidden(self):
+        return isinstance(self, Hidden)
+
+    def is_blind(self):
+        return isinstance(self, Blind)
+
+    def is_percival(self):
+        return isinstance(self, Percival)
+
+    def is_merlin(self):
+        return isinstance(self, Merlin)
+
 class Leader(Role):
     '''Visible to Percival'''
     pass
@@ -17,17 +41,17 @@ class VanRes(Res):
         self.role = R.VANRES
 
 class Merlin(Res, Leader):
-    '''Can see all non-Hidden Spies'''
+    '''Can see all (non-Hidden; i.e. Mordred) Spies'''
     def __init__(self):
         self.role = R.MERLIN
 
 class Percival(Res):
-    '''Can identify Leaders, one of whom is Merlin'''
+    '''Can identify Leader(s), one of whom is Merlin'''
     def __init__(self):
         self.role = R.PERCIVAL
 
 class Spy(Role):
-    '''Can see all non-Oberon Spies'''
+    '''Can see all (non-Blind; i.e. Oberon) Spies'''
     pass
 
 class VanSpy(Spy):
@@ -43,7 +67,7 @@ class Assassin(Shooter):
         self.role = R.ASSASSIN
 
 class Morgana(Spy, Leader):
-    '''Looks like Merlin to Percival'''
+    '''Looks like Merlin (i.e. one of two Leaders) to Percival'''
     def __init__(self):
         self.role = R.MORGANA
 
@@ -55,11 +79,14 @@ class Mordred(Hidden):
     def __init__(self):
         self.role = R.MORDRED
 
-class MordAss(Mordred, Assassin):
+class MordAss(Hidden, Shooter):
     def __init__(self):
         self.role = R.MORDASS
 
-class Oberon(Spy):
-    '''Cannot be seen by other Spies, but can be seen by Merlin'''
+class Blind(Spy):
+    '''Cannot see or be seen by other Spies (but still visible to Merlin)'''
+    pass
+
+class Oberon(Blind):
     def __init__(self):
         self.role = R.OBERON
