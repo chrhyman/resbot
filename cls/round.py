460a549189f39bf59ee02f96a892f16a9a667cf3
chrhyman <chris@wugs.me>
2021-03-21 03:27:35 -0700

    Update round.py

--- cls/round.py
+++ cls/round.py
@@ -1,5 +1,11 @@
+from cls.vote import Vote
+
 class Round:
-    pass
+    def __init__(self, leader):
+        self.leader = leader
+        self.proposed_team = None
+        self.team_vote = Vote()
+        self.approved = None
 
 class RoundList(list):
     pass
