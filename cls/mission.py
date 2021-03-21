4525748a65c338395192af10a073b2c338ad1b1a
chrhyman <chris@wugs.me>
2021-03-21 03:23:12 -0700

    Update mission.py

--- cls/mission.py
+++ cls/mission.py
@@ -1,4 +1,20 @@
-#class Mission
-#class MissionList(list)
+from cls.round import Round, RoundList
+from cls.vote import Vote
+from cls.util import GameError
+
+class Mission:
+    def __init__(self, num):
+        if not (1 <= num <= 5):
+            raise GameError("Mission must be in range 1-5 inclusive.")
+        self.num = num
+        self.rounds = RoundList()
+        self.approved_team = None
+        self.mission_vote = Vote()
+        self.outcome = None
+
+    # TODO: determine what the __str__(self) method should do, if anything
+
+class MissionList(list):
+    pass
 
 # methods to handle checking for needing teams, votes, etc.
