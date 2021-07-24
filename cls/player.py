5f12329efb1b94a6874a723a5423c5a68d6bfe9b
chrhyman <chris@wugs.me>
2021-07-24 00:52:18 -0400

    type hints and formatting

--- cls/player.py
+++ cls/player.py
@@ -1,44 +1,42 @@
 from random import shuffle
+from typing import Optional, Union
 
-from cls.role import Role
+from discord.abc import User
+
+from cls.roles import Role
 from cls.util import Caseless, GameError
 
 class Player:
-    def __init__(self, discord_user):       # arg=discord.py User model object
-        self.nameid = str(discord_user)     # str, 'discord_username#id'
-        self.id = discord_user.id           # int, discord_unique_id for PMs
-        self.nick = discord_user.name       # str, 'discord_username'
-        self.role = None                    # Role object
-        self.ready = False                  # flag for player ready state
+    def __init__(self, discord_user: User):
+        self.discord_user = discord_user
+        self.name = discord_user.name
+        self.unique_name = str(discord_user)    # 'name#discriminator'
+        self.nick = self.name                   # default to username
+        self.role = None
+        self.ready = False                      # flag for intializing the game
 
     def __str__(self):
         return self.nick
 
-    def assign_role(self, role):
-        if self.role is not None:
-            raise GameError(f"Role is already {self.role}")
-        elif not isinstance(role, Role):
-            raise GameError("Must assign role of type Role")
-        else:
-            self.role = role
+    def assign_role(self, role: Role) -> None:
+        self.role = role
 
-    def update_nick(self, new_nick):
+    def update_nick(self, new_nick: str) -> None:
         self.nick = new_nick
 
 class PlayerList(list):
-    def __contains__(self, pl):
-        if isinstance(self.get_player(pl), Player):
+    def __contains__(self, pl: Union[Player, str]) -> bool:
+        if self.get_player(pl) is not None:
             return True
         return False
 
     def __str__(self):
         return self.show(', ')
 
-    def all_players_ready(self):
-        ready_flags = [player.ready for player in self]
-        return all(ready_flags)     # True iff all ready flags are True
+    def all_players_ready(self) -> bool:
+        return all([player.ready for player in self])
 
-    def change_nick(self, player_str, new_nick):
+    def change_nick(self, player_str: str, new_nick: str) -> None:
         if new_nick in self:
             raise GameError("That name is already in use")
         elif player_str not in self:
@@ -46,45 +44,37 @@ class PlayerList(list):
         else:
             self.get_player(player_str).update_nick(new_nick)
 
-    def get_player(self, pl):
+    def get_player(self, pl: Union[Player, str]) -> Optional[Player]:
         for player in self:
-            matches = [player.nameid, str(player.id), player.nick]
             if isinstance(pl, Player) and pl == player:
                 return player
-            elif isinstance(pl, str) and pl in Caseless(matches):
+            matches = [player.name, player.unique_name , player.nick]
+            if isinstance(pl, str) and pl in Caseless(matches):
                 return player
         return None
 
-    def show(self, delimiter):
-        return delimiter.join([str(p) for p in self])
+    def show(self, delimiter: str) -> str:
+        return delimiter.join([str(player) for player in self])
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         shuffle(self)
 
-    def unready_all_players(self):
+    def unready_all_players(self) -> None:
         for player in self:
             player.ready = False
 
 class GamePlayerList(PlayerList):
-    def __init__(self, lst=[]):
+    def __init__(self, lst: Optional[list[Player]]=None):
+        if lst is None:
+            lst = []
         super().__init__(lst)
         self.leader = 0
 
-    def show(self, delimiter):
+    def show(self, delimiter: str) -> str:
         names = [str(p) for p in self]
         names[self.leader] = f"*{names[self.leader]}*"
         return delimiter.join(names)
 
     def shuffle(self):
-        pass # override bc order now matters; shouldn't call on GamePlayerList
-
-# TEMPORARY
-class DU:
-    ID = 0
-    def __init__(self, name):
-        self.name = name
-        self.id = DU.ID
-        DU.ID += 1
-
-    def __str__(self):
-        return f"{self.name}#{self.id}"
+        # order matters; don't call shuffle on GamePlayerList
+        pass
